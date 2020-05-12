d2cf1a57b3e93d4741c261462c82ba1a780660bc
govind-dharamkari <dharamkari.angular@gmail.com>
2020-05-13 01:19:18 +0530

    Feature 2 is completed

--- onlinebookstore/src/main/java/com/dgr/onlinebookstore/config/RepositoryConfig.java
+++ onlinebookstore/src/main/java/com/dgr/onlinebookstore/config/RepositoryConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
+
 @Configuration
 public class RepositoryConfig implements RepositoryRestConfigurer{
 	
